1d40834d592869b32667d72f62765f064e69f857
fedealesandro <fede.alesandro@gmail.com>
2020-09-23 20:08:12 -0300

    Refactor

--- demo/src/main/java/utn/poc/models/User.java
+++ demo/src/main/java/utn/poc/models/User.java
@@ -34,9 +34,6 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "dni")
-    private String dni;
-
     @Column(name = "user_name")
     private String userName;
 
